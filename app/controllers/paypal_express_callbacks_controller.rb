5d81139fc9a7fc28cf62077930ac29047b113b87
jplecaille <jplecaille@gmail.com>
2010-10-12 16:56:43 +0200

    No exception on paypal IPN - TODO

--- app/controllers/paypal_express_callbacks_controller.rb
+++ app/controllers/paypal_express_callbacks_controller.rb
@@ -1,5 +1,9 @@
 class PaypalExpressCallbacksController < Admin::BaseController
+  protect_from_forgery :except => :index
+  
   def index
+    logger.info "Paypal Express callback : #{params.inspect}"
+    
     render :text => "index"
   end
   def show
